0ae32addbba4f8cdc80374c8c098572daa024c32
societyladen <denis.society@ya.ru>
2026-03-17 08:32:56 +0500

    Program1.fs

--- Program1.fs
+++ Program1.fs
@@ -1,24 +1,32 @@
 open System
 
 let readFloatsSeq count =
-    
     seq {
-        for index in 1 .. count do
+        for index = 1 to count do
             printf "Введите вещественное число %d: " index
-            yield Console.ReadLine()
+            let input = Console.ReadLine()
+            
+            let success, value = Double.TryParse input
+            if success then
+                yield value
+            else
+                printfn "  Ошибка: некорректный ввод! Используется 0"
+                yield 0
     }
     |> Seq.map (fun input -> int (float input)) 
 
 [<EntryPoint>]
 let main args =
     printf "Введите количество значений последовательности: "
-    let count = Console.ReadLine() |> int
-
-    if count <= 0 then
-        printf "Ошибка: количество должно быть положительным!"
+    let countInput = Console.ReadLine()
+    
+    let success, count = Int32.TryParse countInput
+    
+    if not success || count <= 0 then
+        printfn "Ошибка: количество должно быть положительным целым числом!"
         exit 1
     else
         let floatsSeq = readFloatsSeq count
-        floatsSeq |> Seq.iter (printfn "Элемент %d")
-
+        floatsSeq |> Seq.iter (printfn "Элемент без дробной части: %d")
+    
     0
