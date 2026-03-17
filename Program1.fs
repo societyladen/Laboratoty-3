61e6c3910eb46e1e930c6c1682bf6eb440e00952
societyladen <denis.society@ya.ru>
2026-03-17 08:11:52 +0500

    Program1.fs

--- Program1.fs
+++ Program1.fs
@@ -1,29 +1,24 @@
-﻿open System
+open System
 
-// Задание 1
 let readFloatsSeq count =
-    [
-    for index in 1 .. count do
-        printf "Введите вещественное число %d: " index
-        let input = Console.ReadLine()
-        yield float input
-    ]
+    
+    seq {
+        for index in 1 .. count do
+            printf "Введите вещественное число %d: " index
+            yield Console.ReadLine()
+    }
+    |> Seq.map (fun input -> int (float input)) 
 
 [<EntryPoint>]
 let main args =
     printf "Введите количество значений последовательности: "
     let count = Console.ReadLine() |> int
-    let list1 = readFloatsSeq count
 
     if count <= 0 then
         printf "Ошибка: количество должно быть положительным!"
         exit 1
     else
-        let floatsSeq = seq{for i in list1 -> i}
-        let intsSeq = floatsSeq
-        let ints = Seq.map int floatsSeq
-        
-        printfn "Последовательность float: %A" intsSeq
-        printfn "Последовательность integer: %A" ints
+        let floatsSeq = readFloatsSeq count
+        floatsSeq |> Seq.iter (printfn "Элемент %d")
 
     0
