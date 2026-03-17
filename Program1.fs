1ec13a6a1fc70993dfcce0a8ceda78e03a013f1f
societyladen <denis.society@ya.ru>
2026-03-17 09:04:03 +0500

    Program1.fs

--- Program1.fs
+++ Program1.fs
@@ -1,32 +1,39 @@
 open System
 
-let readFloatsSeq count =
+let rec readFloatsSeq count =
     seq {
         for index = 1 to count do
-            printf "Введите вещественное число %d: " index
-            let input = Console.ReadLine()
+            let rec readValue() =
+                printf "Введите вещественное число %d: " index
+                let input = Console.ReadLine()
+                
+                let success, value = Double.TryParse input
+                if success then
+                    value
+                else
+                    printfn "  Ошибка: некорректный ввод! Попробуйте снова."
+                    readValue()
             
-            let success, value = Double.TryParse input
-            if success then
-                yield value
-            else
-                printfn "  Ошибка: некорректный ввод! Используется 0"
-                yield 0
+            yield readValue()
     }
-    |> Seq.map (fun input -> int (float input)) 
+    |> Seq.map int
+
+let rec getValidCount() =
+        printf "Введите количество значений последовательности: "
+        let countInput = Console.ReadLine()
+        
+        let success, count = Int32.TryParse countInput
+        
+        if not success || count <= 0 then
+            printfn "Ошибка: количество должно быть положительным целым числом! Попробуйте снова."
+            getValidCount()
+        else
+            count
 
 [<EntryPoint>]
 let main args =
-    printf "Введите количество значений последовательности: "
-    let countInput = Console.ReadLine()
-    
-    let success, count = Int32.TryParse countInput
-    
-    if not success || count <= 0 then
-        printfn "Ошибка: количество должно быть положительным целым числом!"
-        exit 1
-    else
-        let floatsSeq = readFloatsSeq count
-        floatsSeq |> Seq.iter (printfn "Элемент без дробной части: %d")
+    let count = getValidCount()
+    let floatsSeq = readFloatsSeq count
+    floatsSeq |> Seq.iter (printfn "Элемент без дробной части: %d")
     
     0
